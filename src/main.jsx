1682c125d11eff8813ec56cfdb1b460cbed8243c
YOGESH233012 <ashutosh.srfml@gmail.com>
2026-04-14 09:33:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { AppProvider } from './context/AppContext'  // 👈 ADD THIS
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppProvider>   {/* 👈 VERY IMPORTANT */}
+      <App />
+    </AppProvider>
+  </React.StrictMode>
 )
